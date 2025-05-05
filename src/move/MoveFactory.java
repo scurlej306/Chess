c1f5ced54d08c37c2069dd32eb03e93bcf04ec5c
Jacob Curley <jacobrcurley@gmail.com>
2025-05-04 19:26:54 -0500

    Fix bug where disambiguation was incomplete. Also resolves issue #9

--- src/move/MoveFactory.java
+++ src/move/MoveFactory.java
@@ -21,6 +21,10 @@ public record MoveFactory(InputDto input, Board board, Team currentTeam) {
         return constructNormalMove();
     }
 
+    private Predicate<Piece> canReachTargetSpace(Space targetSpace) {
+        return piece -> MoveValidator.isValid(piece, targetSpace, board);
+    }
+
     private CastleMove constructCastleMove() {
         int kingColChange;
         int rookColChange;
@@ -63,27 +67,23 @@ public record MoveFactory(InputDto input, Board board, Team currentTeam) {
 
     private Move constructNormalMove() {
         Space targetSpace = board.getSpace(input.destinationCol, input.destinationRow);
-        char token = input.movingPieceToken != null ? input.movingPieceToken : 'P';
         Set<Piece> validPieces = currentTeam.stream().filter(
-                        piece -> piece.getToken() == token && MoveValidator.isValid(piece, targetSpace, board))
+                        matchesToken().and(matchesDisambiguator()).and(canReachTargetSpace(targetSpace)))
                 .collect(Collectors.toSet());
+
         if (validPieces.isEmpty()) {
             throw new IllegalArgumentException("No piece of the provided type can reach the target space");
         }
-        Piece targetPiece;
         if (validPieces.size() > 1) {
-            Predicate<Piece> pieceMatcher = getPiecePredicate();
-            targetPiece = validPieces.stream().filter(pieceMatcher).findFirst()
-                    .orElseThrow(() -> new IllegalArgumentException("Ambiguous move - multiple pieces of the provided" +
-                                                                    " type can reach the target space"));
-        } else {
-            targetPiece = validPieces.iterator().next();
+            throw new IllegalArgumentException("Ambiguous move - multiple pieces of the provided" +
+                                               " type can reach the target space");
         }
 
+        Piece targetPiece = validPieces.iterator().next();
         return new Move(targetPiece, targetSpace);
     }
 
-    private Predicate<Piece> getPiecePredicate() {
+    private Predicate<Piece> matchesDisambiguator() {
         if (input.originCol != null && input.originRow != null) {
             Space origin = board.getSpace(input.originCol, input.originRow);
             return piece -> piece.getCurSpace().equals(origin);
@@ -94,6 +94,11 @@ public record MoveFactory(InputDto input, Board board, Team currentTeam) {
         if (input.originRow != null) {
             return piece -> piece.getCurSpace().getRow() == input.originRow;
         }
-        return _ -> false;
+        return _ -> true;
+    }
+
+    private Predicate<Piece> matchesToken() {
+        char token = input.movingPieceToken != null ? input.movingPieceToken : 'P';
+        return piece -> piece.getToken() == token;
     }
 }
